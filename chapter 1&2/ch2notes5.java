6391c8639a0686e639110f7ecb52562a796162d2
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2021-08-30 14:49:41 -0500

    finished ch2 notes5

--- chapter 1&2/ch2notes5.java
+++ chapter 1&2/ch2notes5.java
@@ -6,7 +6,7 @@
 //Program
 //Description
 //Who helped me: Mr. Hayes
-
+//this is the package
 import java.util.Random; // lets us use the random class
 //you import the Scanner Class!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 import java.util.Scanner; // last word is the the Class
@@ -48,20 +48,23 @@ public static void main (String[] args)
         Integer number = new Integer(47);
         String myname = new String("Kevin Hayes");
         Integer n2 = 7;//Auto Boxing
-/**/
+/* */
         String word = new String("Homer Simpson");
         String word2 = new String("Homer Simpson");
         String word3 = new String("Bart Simpson");
-        String name = new String("Mike");
+        String name = new String();
+        //string is special other classes/objects can't do this
         String last = "Hayes";
-
-
-/*
+    //    System.out.println(word.length());
+      //  System.out.println("the name is " + name);
+/* 
         System.out.printf("Name:  %s \t ID: %5d \n", name,id);
-        System.out.println( 'a' + 100 );
+        System.out.println( 'a' + 100 ); // chars are ints
         System.out.println('a'   );
-        System.out.println("A"+100  );
-
+        System.out.println("A"+100);
+        System.out.println("The add is "+ (7 + 5));
+        System.out.println(7 + 5 + " is the add");
+*/
 //      System.out.printf(  );
 
 //      System.out.printf( );
@@ -71,44 +74,51 @@ public static void main (String[] args)
     System.out.println( );
     System.out.println(  );
 
-/*
+
 
         num1 = 23.89765; num2 = 3.4;
 //Your create a NumberFormat and Decimalformat object
 
-
+/*
         NumberFormat money = NumberFormat.getCurrencyInstance();
         NumberFormat percent = NumberFormat.getPercentInstance();
-        DecimalFormat fmt =new DecimalFormat("0.###");
-        DecimalFormat fmt2= new DecimalFormat("0.00");// ## vs 00
+        DecimalFormat fmt =new DecimalFormat("0.###"); // max round floats
+        DecimalFormat fmt2= new DecimalFormat("0.0000");// ## vs 00
         
 
         System.out.println( "Sub Total: " + money.format(num1) );
-        System.out.println( "Sub Total: " + percent.format(num1) );
+        System.out.println( "Sub Total: " + percent.format(.5) );
         System.out.println( "Sub Total: " + fmt.format(num1) );
-        System.out.println( "Sub Total: " + fmt2.format(num2) );
-
-
+        System.out.println( "Sub Total: " + fmt2.format(num2) ); // pads with 0s 
+        
+        System.out.println( "Sub Total: " + fmt.format(num2) );
+        System.out.println( "Sub Total: " + fmt2.format(num1) );
+*/
 // page 90 Why do we get this Error???
 /*
         System.out.print("Enter a number ");
         x = Keyboard.nextInt   ();
         System.out.println(x  );
-        System.out.print("Enter a word ");
-        //word = Keyboard.next   ();
+        System.out.println("Enter a word ");
+        Keyboard.nextLine ();
+        word = Keyboard.next   ();
         word = Keyboard.nextLine   ();
+        
 
-/*
-        System.out.print("Enter a decimal");
+
+        System.out.print("Enter 3 numbers");
+        x = Keyboard.nextInt   ();
+        y = Keyboard.nextInt   ();
+        z = Keyboard.nextInt   ();
         num = Keyboard.nextDouble ();
-        System.out.println(num  );
-
+        System.out.println(x + " "+" " + y + " " + z  );
+*/
 
         // what happens when you have both of these together
-/*
-        System.out.print("Enter a word");
-        Keyboard.nextLine();
-        word = Keyboard.nextLine();
+/* 
+        System.out.println("Enter a word");
+        word = Keyboard.next(); // next is until the next white space, while next line is the next line
+       // word = Keyboard.nextLine();
         System.out.println(word  );
 
 /*      System.out.println( "enter a word" );
@@ -122,37 +132,45 @@ public static void main (String[] args)
 
 
 
-/*
+/* 
 //  Can we make a Math Object? What type of class is Math???
-      Math silly = new Math();
+  //    Math silly = new Math(); // do not make objects from static classes 
+      //pg 88 
 
-        System.out.println(Math.abs(-4));
+        System.out.println(Math.abs(-4)); // returns a number 
         System.out.println(Math.sqrt (12));
         System.out.println(Math.pow(2,3));
 //      System.out.println(silly.pow(2,4));
 
 
-/*
+/* 
 //!!!!!!!!!!!! Make a Random Object called generator
         Random generator = new Random();
-
+     // 100 numbers from 0-99
         x = generator.nextInt(100);
         System.out.println("the Random number is " + x);
-
+     // decimal of 17 digits 0 - 1 exculsive
         num =generator.nextDouble()*50;
         System.out.println(num);
-/*
+        num =Math.random()* 100 - 50;
+        System.out.println(num);
+        /*
 
-/*
+/**/
 //----------------------------Know it live it love it p 78
         //word is an oject can do things (NOT len(word))
         System.out.println(  word.length() );
         System.out.println(word == word2 );
+        System.out.println(word.equals(word2));
         System.out.println( x == 9 );
         System.out.println(  word.compareTo(word3 ));
         System.out.println(  word.indexOf("p"));
-        
-        
+        //slice up string - substring
+        //substring(index returns string from index to end
+        System.out.println(  word.substring(word.indexOf("r")));
+        //substrin(start, end);
+        int find = word.indexOf("S");
+        System.out.println(   word.substring(find,find+2));
 
 
 
@@ -162,22 +180,26 @@ public static void main (String[] args)
 //System.out.println((int)(letter + letter ) );
 
     //System.out.println( (char)101 );
-/**/
-        double conversionFactor = (double)x/5; //9/5 logic error -- promotion 5.0
-        System.out.println(conversionFactor);
-        final int BASE = 32;            // cast is (data type)
-        int celsiusTemp = 30;
+/*
+x = 9;
+y = 5;
+   // double conversionFactor = 9.0/5; //9/5 logic error -- promotion changing 9 to 9.0 (int to double)
+    double conversionFactor = (double)x/y; // casting (temporarily widening data type)
+    System.out.println(conversionFactor); // int/int = int
+     final int BASE = 32;            // cast is (data type)
+      int celsiusTemp = 30;
 
         double fahrenTemp = celsiusTemp * conversionFactor + BASE;
 
         System.out.println("Celsius Temp: " + celsiusTemp );
         System.out.println("Fahrenheit Equivalent" + fahrenTemp  );
-
+  
+        System.out.println((int)(Math.random() * 100));
 /*
-    //num = num + num;
-    //num = num - num;
-    //num = num * num;
-    //num = num / num;
+    num = num + num;
+    num = num - num;
+    num = num * num;
+    num = num / num;
     num = num % 3;
     System.out.println(num);
 
@@ -188,20 +210,3 @@ public static void main (String[] args)
 }//end of main
 
 }//end of class
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
