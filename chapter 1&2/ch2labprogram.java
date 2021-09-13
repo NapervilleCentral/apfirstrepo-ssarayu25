54667aafa830c99a8edd0eeccc0b0a11c34b618b
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2021-09-13 17:41:10 -0500

    fixed ch2labprogram

--- chapter 1&2/ch2labprogram.java
+++ chapter 1&2/ch2labprogram.java
@@ -1,5 +1,5 @@
 // Name: Sarayu Suresh
-// 9/2/21
+// 9/2/21 and 9/13/21
 // Title: Name program
 // Input full name, prints first, middle inital, and last on seperate lines 
 import java.util.Scanner;
@@ -9,16 +9,20 @@ public class ch2labprogram
   public static void main (String[] args)
   {
       Scanner Keyboard = new Scanner(System.in);
+      
       System.out.println("Enter Full Name: ");
+      String name = Keyboard.nextLine();
       
-      String firstname = Keyboard.next();
-      String middlename = Keyboard.next();
-      String lastname = Keyboard.next();
-   
-      String middleinitial = middlename.substring(0,1);
+      //splitting name into two parts 
+      int find = name.lastIndexOf(" ");
+      String lastname = name.substring(find + 1); // contains only last name
+      int find2 = name.indexOf(" ");
+      String firstname = name.substring(0,(find2 + 2)); // contains first name and middle initial
       
-      System.out.println(firstname + " " + middleinitial + ".");
-      System.out.println(lastname);
+      System.out.println(firstname + ".");
+      System.out.print(lastname);
+
+
       
       
   }
